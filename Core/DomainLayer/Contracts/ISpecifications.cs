8d30fdee954dfdb12308e0e632bb4026c930cab8
saadmostafa1174@gmail.com <saadmostafa1174@gmail.com>
2025-04-23 02:02:41 +0200

    Specifications

---
+++ Core/DomainLayer/Contracts/ISpecifications.cs
@@ -0,0 +1,17 @@
+﻿using DomainLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Contracts
+{
+    public interface ISpecifications<TEntity , TKey> where TEntity : BaseEntity<TKey>
+    {
+        // Property Signature For Each Dynamic Part int Query
+        public Expression<Func<TEntity,bool>> Criteria { get; }
+        List<Expression<Func<TEntity, object>>> IncludeExpressions { get; }
+    }
+}
